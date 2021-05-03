351269813b4d5860105d135258abd1e84a3d35a3
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-05-03 13:09:28 -0700

    Add functional ping pong game
    Co-authored-by: Brent Hubbard <hubbardbrent@hotmail.com>

---
+++ Models/Ping-Pong.cs
@@ -0,0 +1,48 @@
+using System;
+
+
+
+   class PingPong
+  {
+
+    static void Main()
+    {
+      Console.WriteLine("Would you like to play Ping-Pong? ['Y' for yes, 'Enter' for no]  ");
+      string answer = Console.ReadLine();
+      if(answer == "Y" || answer == "y")
+      {
+         PlayGame(); 
+      }
+      else
+      {
+        Console.WriteLine("Welp nevermind then");
+      }
+
+    }
+
+    static void PlayGame()
+    {
+      Console.WriteLine("Enter a number from 1-30");
+      string stringUserInput = Console.ReadLine();
+      int userInput = int.Parse(stringUserInput);
+      for( int number = 0; number <= userInput; number++)
+        if( number % 3 == 0 && number % 5 == 0){
+        Console.WriteLine("Ping");
+      }
+      else if ( number % 3 == 0)
+      {
+        Console.WriteLine("Pong");
+      }
+      else if ( number % 5 == 0)
+      {
+        Console.WriteLine("Ping Pong");
+      }
+      else 
+      {
+        Console.WriteLine(number);
+      }
+
+
+  } 
+
+  }
